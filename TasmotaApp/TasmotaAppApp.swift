3cadc60b18c08e5fbc08fe0726246a0d89c8c1ae
saihgupr <121105543+saihgupr@users.noreply.github.com>
2025-08-20 16:31:24 -0400

    Initial Commit

---
+++ TasmotaApp/TasmotaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasmotaAppApp.swift
+//  TasmotaApp
+//
+//  Created by Chris LaPointe on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TasmotaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
